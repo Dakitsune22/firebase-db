ee05434616ce09267d747cba7a1b10e617eab6f0
Dakitsune22 <dakdak1981@gmail.com>
2025-06-10 18:37:05 +0200

    Added function to get age from DD/MM/YYYY string date.

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,3 +1,17 @@
 export const sleep = async (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
+
+export const getAge = (birthDateStr: string): number => {
+  // Mask expected: DD/MM/YYYY
+  const birthDateArray = birthDateStr.split('/');
+  const birthDate = new Date(
+    Number(birthDateArray[2]),
+    Number(birthDateArray[1]) - 1,
+    Number(birthDateArray[0])
+  );
+
+  const diffMS = Date.now() - birthDate.getTime();
+  const ageDate = new Date(diffMS);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
